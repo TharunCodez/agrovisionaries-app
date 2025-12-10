02ea5423ae93b7de0039fef3460839e103d8484e
V. Tharun <hitmantharun@gmail.com>
2025-12-10 05:03:06 +0000

    Try fixing this error: `Runtime TypeError: Cannot use 'in' operator to s

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -113,16 +113,24 @@ CommandSeparator.displayName = CommandPrimitive.Separator.displayName
 const CommandItem = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
->(({ className, ...props }, ref) => (
-  <CommandPrimitive.Item
-    ref={ref}
-    className={cn(
+>(({ className, ...props }, ref) => {
+  if (!ref) {
+    return <CommandPrimitive.Item className={cn(
       "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50",
       className
-    )}
-    {...props}
-  />
-))
+    )} {...props} />
+  }
+  return (
+    <CommandPrimitive.Item
+      ref={ref}
+      className={cn(
+        "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50",
+        className
+      )}
+      {...props}
+    />
+  )
+})
 
 CommandItem.displayName = CommandPrimitive.Item.displayName
 
