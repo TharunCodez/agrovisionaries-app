b6776a5c695073281a0859ce667ee292b781c64d
V. Tharun <hitmantharun@gmail.com>
2025-12-11 09:27:57 +0000

    Try fixing this error: `Unhandled Runtime Error: [ Server ] Error: "proj

--- src/app/api/farmer-data.ts
+++ src/app/api/farmer-data.ts
@@ -2,9 +2,17 @@
 
 import { getFirestore, collection, query, where, getDocs, doc, getDoc } from 'firebase/firestore';
 import { initializeApp, getApps } from 'firebase/app';
-import { firebaseConfig } from '@/firebase';
 import { normalizeFirestoreData } from '@/lib/normalizeFirestoreData';
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5TJ8aLFr1YCeDR-PTish_madUiMqJBB4",
+  authDomain: "studio-9156404750-b637f.firebaseapp.com",
+  projectId: "studio-9156404750-b637f",
+  storageBucket: "studio-9156404750-b637f.appspot.com",
+  messagingSenderId: "545867839750",
+  appId: "1:545867839750:web:1fa08a994b50d2f7fa4687"
+};
 
 // Helper to initialize Firebase App on the server for actions
 const getDb = () => {
