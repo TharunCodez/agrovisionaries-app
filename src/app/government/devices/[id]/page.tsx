3a23cb354e5fbd8f800313749dd32d8823a2be13
V. Tharun <hitmantharun@gmail.com>
2026-01-03 13:29:24 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/government/devices/[id]/page.tsx
+++ src/app/government/devices/[id]/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { Suspense } from 'react';
+import { Suspense, use } from 'react';
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import {
   Card,
